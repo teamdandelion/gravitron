7a20b12925cac814287f65e47f71b7f2e4eab5c2
Daniel Mane <danmane@gmail.com>
2014-09-28 18:37:41 -0700

    Initial work on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,8 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
 
 gulp.task('default', function() {
-
+  var tsResult = gulp.src('src/*.ts')
+                     .pipe(ts({}));
+  tsResult.ts.pipe(gulp.dest('build'))
 });
